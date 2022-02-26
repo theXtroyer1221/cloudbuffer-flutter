cacb33df1c2c99f9e4cf89b1fc536a2b4f38bc1b
theXtroyer1221 <jaddou2005@gmail.com>
2022-02-26 01:24:03 +0100

    added base functionality and also api calls for weather

--- lib/main.dart
+++ lib/main.dart
@@ -1,115 +1,171 @@
+// ignore_for_file: avoid_print
+
+import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
+import 'dart:convert';
+import 'secrets.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+        title: "Cloud buffer",
+        theme: ThemeData(
+            colorScheme: ColorScheme.fromSwatch().copyWith(
+                primary: const Color(0xFF283655),
+                secondary: const Color(0xFF4facfe)),
+            fontFamily: "Georgia"),
+        home: Scaffold(
+          appBar: AppBar(
+            centerTitle: true,
+            title: Image.asset(
+              "logo-hori.png",
+              width: 200,
+              fit: BoxFit.contain,
+            ),
+          ),
+          body: const HomePage(),
+        ));
+  }
+}
+
+class City {
+  final String name;
+  final String desc;
+  final String country;
+  final double temp;
+  final double windSpeed;
+
+  const City({
+    required this.name,
+    required this.desc,
+    required this.country,
+    required this.temp,
+    required this.windSpeed,
+  });
+
+  factory City.fromJson(Map<String, dynamic> json) {
+    return City(
+      name: json['name'],
+      desc: json['weather'][0]['description'],
+      country: json['sys']["country"],
+      temp: json['main']['temp'],
+      windSpeed: json['wind']['speed'],
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
+Future<City> fetchWeather(search) async {
+  final response = await http.get(Uri.parse(
+      'https://api.openweathermap.org/data/2.5/weather?q=$search&appid=$API_KEY&units=metric'));
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  if (response.statusCode == 200) {
+    return City.fromJson(jsonDecode(response.body));
+  } else {
+    throw Exception('Failed to load the weather');
+  }
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final _formKey = GlobalKey<FormState>();
+
+  String query = "22";
+  late Future<City> futureCity = fetchWeather("london");
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+    return Container(
+      alignment: Alignment.topCenter,
+      margin: const EdgeInsets.symmetric(horizontal: 10, vertical: 50),
+      child: Column(
+        children: [
+          const Text(
+            "The Clouds reach every corner in the world",
+            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 40),
+            textAlign: TextAlign.center,
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          const Text(
+              "Your search too, try finding your city's weather anywhere, at anytime",
+              style: TextStyle(fontWeight: FontWeight.w600, fontSize: 15)),
+          const SizedBox(
+            height: 50,
+          ),
+          FutureBuilder<City>(
+            future: futureCity,
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                return Column(
+                  children: [
+                    Text(snapshot.data!.name),
+                    Text(snapshot.data!.desc),
+                    Text('${snapshot.data!.temp.round().toString()} °c'),
+                    Text('${snapshot.data!.windSpeed.toString()} m/s'),
+                  ],
+                );
+              } else if (snapshot.hasError) {
+                return Text('${snapshot.error}');
+              }
+              return const CircularProgressIndicator();
+            },
+          ),
+          SizedBox(
+            width: 300,
+            child: Form(
+                key: _formKey,
+                child: Column(
+                  children: [
+                    TextFormField(
+                      decoration: const InputDecoration(
+                          hintText: "Ex. London, New York, Paris",
+                          labelText: "Search a city name"),
+                      onChanged: (value) => query = value,
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return "You cant leave the field empty";
+                        }
+                        return null;
+                      },
+                    ),
+                    const SizedBox(height: 10),
+                    ElevatedButton(
+                        onPressed: () async {
+                          if (_formKey.currentState!.validate()) {
+                            ScaffoldMessenger.of(context).showSnackBar(
+                                const SnackBar(
+                                    content:
+                                        Text("Searching for the weather...")));
+                            setState(() {
+                              futureCity = fetchWeather(query);
+                            });
+                          }
+                        },
+                        child: const Text("Submit"))
+                  ],
+                )),
+          )
+        ],
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
